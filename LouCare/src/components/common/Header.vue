f4ab82d986da8c3769e0c972b0ca96021579ab61
Osvaldo Rodriguez <osvaldo.rguez911001@gmail.com>
2025-10-17 09:23:20 -0400

    Add new content and some styling to the Hero.vue, Header.vue, and StreetTipsView.vue

--- LouCare/src/components/common/Header.vue
+++ LouCare/src/components/common/Header.vue
@@ -3,8 +3,8 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <header class="bg-black text-white fixed top-0 left-0 z-10 flex justify-around items-center w-full p-4">
-    <img src="/logo.png" alt="Lou Cares" class="w-30 h-30" />
+  <header class="bg-gradient-to-b from-black/90 to-black/20 backdrop-blur-sm text-white fixed top-0 left-0 z-10 flex justify-around items-center w-full p-4">
+    <img src="/logo.png" alt="Lou Cares" class="w-30 h-30 animate-fade-in-right animate-delay-100" />
     <nav class="flex justify-center items-center">
       <ul class="flex justify-around items-center gap-10">
         <li class="hover:text-yellow-400 cursor-pointer transition-all duration-200"><RouterLink to="/">Home</RouterLink></li>
--- LouCare/src/views/home/sections/Hero.vue
+++ LouCare/src/views/home/sections/Hero.vue
@@ -3,12 +3,12 @@
 </script>
 
 <template>
-    <div class="flex items-center justify-center w-full h-screen p-10 gap-10 overflow-hidden">
-        <div class="flex flex-col items-start justify-center gap-12 h-full">
+    <div class="flex items-center justify-start w-full h-screen p-10 overflow-hidden relative">
+        <div class="flex flex-col items-start justify-center gap-12 pl-20">
             <h1>Get the help you need <br> when you need it</h1>
             <p>Find information on places offering free meals, <br> safe rest, and community support</p>
-            <button class="bg-blue-800 text-white mt-20 px-6 py-4 rounded-md hover:bg-blue-900 cursor-pointer hover:scale-105 transition-all duration-200 animate-blurred-fade-in delay-100">Get Started</button>
+            <button class="bg-blue-800 text-white mt-20 px-6 py-4 rounded-md hover:bg-blue-900 cursor-pointer hover:scale-105 transition-all duration-200 animate-blurred-fade-in animate-delay-400">Get Started</button>
         </div>
-        <img src="" alt="Person" class="h-full object-fit"/>
+        <img src="/smiling-person.png" alt="Person" class="absolute -right-40 bottom-0 -z-10 w-3/4 h-full object-cover"/>
     </div>
 </template>
